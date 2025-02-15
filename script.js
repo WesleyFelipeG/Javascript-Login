09a3eb50458fc79e3a8ec29c9d9aec7f05e573ae
Wesley Felipe Gouveia Soares <wesleysoares1337@gmail.com>
2025-02-15 18:21:41 -0300

    Atividade 2/3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//---------------------------------------------------------------------------------
+// Password visibility toggle
 function showPassword() {
     var passwordBox = document.getElementById('password')
     var showPasswordButton = document.getElementById('showPasswordButton')
@@ -12,3 +14,16 @@ function showPassword() {
         showPasswordButton.classList.add('bxs-lock-alt')
     }
 }
+//---------------------------------------------------------------------------------
+// "Hello [user]!" message
+document.getElementById('loginScreen').addEventListener('submit', function (u) {
+    u.preventDefault();
+
+    var user = document.getElementById('user').value;
+    if (user) {
+        document.getElementById('loginScreen').style.display = 'none';
+        var successfulLoginMessage = document.getElementById('successfulLoginMessage');
+        document.getElementById('helloUser').textContent = user;
+        successfulLoginMessage.style.display = 'block';
+    }
+});
